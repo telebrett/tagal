2ea02a3504f059c095c8b3315a8f3c2db512560d
Brett Gardner <bgardner@noggin.com.au>
2021-05-30 09:41:38 +1000

    Upgraded to 11

--- angular/src/app/varousel/varousel.component.spec.ts
+++ angular/src/app/varousel/varousel.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VarouselComponent } from './varousel.component';
 
@@ -6,7 +6,7 @@ describe('VarouselComponent', () => {
   let component: VarouselComponent;
   let fixture: ComponentFixture<VarouselComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VarouselComponent ]
     })
